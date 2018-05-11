093517a856b94b90096f2a340f767bd86d63d4a5
Jed Stevens <jed.w.stevens@gmail.com>
2018-05-11 14:10:59 -0400

    AutoCommit

--- chat.gd
+++ chat.gd
@@ -6,13 +6,11 @@ func on_input_pressed():
 	rpc("send_message", $input.text)
 
 sync func send_message(msg):
-	print("loop")
 	rpc("log_text", msg)
 
-sync func log_text(msg):
-	print("log")
-	$log.text = $log.text + "\n" + msg
+sync func log_text(msg, id_from):
+	var header = $log.text + "\n" + get_parent().player_info[from_id]['name'].to_upper()
+	$log.text = header + ":  " +msg
 
 func _on_send_pressed():
-	print("signal")
 	send_message($input.text)
