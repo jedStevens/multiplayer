8b96778efb74ccb3a8d05f01b961e8724699bed3
Jed Stevens <jed.w.stevens@gmail.com>
2018-05-11 14:07:18 -0400

    AutoCommit

--- chat.gd
+++ chat.gd
@@ -6,4 +6,13 @@ func on_input_pressed():
 	rpc("send_message", $input.text)
 
 sync func send_message(msg):
+	print("loop")
+	rpc("log_text", msg)
+
+sync func log_text(msg):
+	print("log")
 	$log.text = $log.text + "\n" + msg
+
+func _on_send_pressed():
+	print("signal")
+	send_message($input.text)
